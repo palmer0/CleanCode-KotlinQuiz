20bd13345569488c06db53c5a233f0ca38212509
Luis Hernandez <lhdez@iuma.ulpgc.es>
2018-12-10 08:56:45 +0000

    refactorizando codigo

--- app/src/main/java/es/ulpgc/eite/cleancode/kotlin/quiz/question/QuestionModel.kt
+++ app/src/main/java/es/ulpgc/eite/cleancode/kotlin/quiz/question/QuestionModel.kt
@@ -1,6 +1,7 @@
 package es.ulpgc.eite.cleancode.kotlin.quiz.question
 
 import QuestionContract
+import android.content.res.Resources
 import android.util.Log
 import es.ulpgc.eite.cleancode.kotlin.quiz.R
 import java.lang.ref.WeakReference
@@ -24,21 +25,29 @@ class QuestionModel(
         Log.d(TAG, "fetchQuestionData()")
 
         activity?.get()?.resources?.let {
+            return _fetchQuestionData(it)
+
+            /*
             var falseLabel = it.getString(R.string.false_label)
             var trueLabel = it.getString(R.string.true_label)
             var nextLabel = it.getString(R.string.next_label)
             var cheatLabel = it.getString(R.string.cheat_label)
 
             return QuestionData(trueLabel, falseLabel, cheatLabel, nextLabel)
+            */
         }
 
         fragment?.get()?.resources?.let {
+            return _fetchQuestionData(it)
+
+            /*
             var falseLabel = it.getString(R.string.false_label)
             var trueLabel = it.getString(R.string.true_label)
             var nextLabel = it.getString(R.string.next_label)
             var cheatLabel = it.getString(R.string.cheat_label)
 
             return QuestionData(trueLabel, falseLabel, cheatLabel, nextLabel)
+            */
         }
     }
 
@@ -46,44 +55,82 @@ class QuestionModel(
         Log.d(TAG, "fetchAnswerData()")
 
         activity?.get()?.resources?.let {
+            return _fetchAnswerData(it)
+
+            /*
             var correctLabel = it.getString(R.string.correct_label)
             var incorrectLabel = it.getString(R.string.incorrect_label)
 
             return AnswerData(correctLabel, incorrectLabel)
+            */
         }
 
         fragment?.get()?.resources?.let {
+            return _fetchAnswerData(it)
+
+            /*
             var correctLabel = it.getString(R.string.correct_label)
             var incorrectLabel = it.getString(R.string.incorrect_label)
 
             return AnswerData(correctLabel, incorrectLabel)
+            */
         }
 
     }
 
+    private fun _fetchQuestionData(resources: Resources) : QuestionData {
+        var falseLabel = resources.getString(R.string.false_label)
+        var trueLabel = resources.getString(R.string.true_label)
+        var nextLabel = resources.getString(R.string.next_label)
+        var cheatLabel = resources.getString(R.string.cheat_label)
+
+        return QuestionData(trueLabel, falseLabel, cheatLabel, nextLabel)
+    }
+
+    private fun _fetchAnswerData(resources: Resources) : AnswerData {
+        var correctLabel = resources.getString(R.string.correct_label)
+        var incorrectLabel = resources.getString(R.string.incorrect_label)
+
+        return AnswerData(correctLabel, incorrectLabel)
+    }
 
     override fun getCurrentAnswer(index: Int): Boolean? {
         activity?.get()?.resources?.let {
-            val answers = it.getStringArray(R.array.answers)
-            return answers.get(index)?.toBoolean()
+            //val answers = it.getStringArray(R.array.answers)
+            //return answers.get(index)?.toBoolean()
+            return _getCurrentAnswer(it, index)
         }
 
         fragment?.get()?.resources?.let {
-            val answers = it.getStringArray(R.array.answers)
-            return answers.get(index)?.toBoolean()
+            //val answers = it.getStringArray(R.array.answers)
+            //return answers.get(index)?.toBoolean()
+            return _getCurrentAnswer(it, index)
         }
     }
 
+
     override fun getCurrentQuestion(index: Int): String? {
         activity?.get()?.resources?.let {
-            val questions = it.getStringArray(R.array.questions)
-            return questions.get(index)
+            //val questions = it.getStringArray(R.array.questions)
+            //return questions.get(index)
+            return _getCurrentQuestion(it, index)
         }
 
         fragment?.get()?.resources?.let {
-            val questions = it.getStringArray(R.array.questions)
-            return questions.get(index)
+            //val questions = it.getStringArray(R.array.questions)
+            //return questions.get(index)
+            return _getCurrentQuestion(it, index)
         }
+    }
+
+    private fun _getCurrentQuestion(resources: Resources, index: Int): String? {
+        val questions = resources.getStringArray(R.array.questions)
+        return questions.get(index)
+    }
+
+    private fun _getCurrentAnswer(resources: Resources, index: Int): Boolean? {
+        val answers = resources.getStringArray(R.array.answers)
+        return answers.get(index)?.toBoolean()
     }
 
     companion object {
--- app/src/main/java/es/ulpgc/eite/cleancode/kotlin/quiz/question/QuestionRouter.kt
+++ app/src/main/java/es/ulpgc/eite/cleancode/kotlin/quiz/question/QuestionRouter.kt
@@ -1,5 +1,7 @@
 package es.ulpgc.eite.cleancode.kotlin.quiz.question
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.support.v4.app.Fragment
 import es.ulpgc.eite.cleancode.kotlin.quiz.app.AppMediator
@@ -12,45 +14,92 @@ class QuestionRouter {
     var activity: WeakReference<QuestionActivity>? = null
 
 
+    private fun _passDataToCheatScreen(mediator: AppMediator, answer: Boolean?) {
+        //val mediator = it.application as AppMediator
+        mediator.answer = answer
+    }
+
     fun passDataToCheatScreen(answer: Boolean?) {
         activity?.get()?.let {
+            _passDataToCheatScreen(it.application as AppMediator, answer)
+
+            /*
             val mediator = it.application as AppMediator
             mediator.answer = answer
+            */
         }
 
         fragment?.get()?.activity?.let {
+            _passDataToCheatScreen(it.application as AppMediator, answer)
+
+            /*
             val mediator = it.application as AppMediator
             mediator.answer = answer
+            */
         }
     }
 
+
+    private fun _getDataFromCheatScreen(mediator: AppMediator): Boolean? {
+        //val mediator = it.application as AppMediator
+        val cheated = mediator.cheated
+        mediator.cheated = null
+        return cheated
+    }
+
+
     fun getDataFromCheatScreen(): Boolean? {
         activity?.get()?.let {
+            return _getDataFromCheatScreen(it.application as AppMediator)
+
+            /*
             val mediator = it.application as AppMediator
+            //return _getDataFromCheatScreen(mediator)
             val cheated = mediator.cheated
             mediator.cheated = null
             return cheated
+            */
         }
 
         fragment?.get()?.activity?.let {
+            return _getDataFromCheatScreen(it.application as AppMediator)
+
+            /*
             val mediator = it.application as AppMediator
+            //return _getDataFromCheatScreen(mediator)
             val cheated = mediator.cheated
             mediator.cheated = null
             return cheated
+            */
         }
 
+    }
+
+
+    private fun _navigateToCheatScreen(mediator: AppMediator) {
+        val context = mediator.baseContext
+        val intent = Intent(context, CheatActivity::class.java)
+        context.startActivity(intent)
     }
 
     fun navigateToCheatScreen() {
 
         activity?.get()?.let {
+            _navigateToCheatScreen(it.application as AppMediator)
+
+            /*
             val intent = Intent(it, CheatActivity::class.java)
             it.startActivity(intent)
+            */
         }
 
         fragment?.get()?.activity?.let {
+            _navigateToCheatScreen(it.application as AppMediator)
+
+            /*
             val intent = Intent(it, CheatActivity::class.java)
             it.startActivity(intent)
+            */
         }
 
         /*
@@ -62,6 +111,7 @@ class QuestionRouter {
         */
 
     }
+
 
     fun determineNextScreen(position: Int): Fragment {
         // based on the position or some other data decide what is the next scene
